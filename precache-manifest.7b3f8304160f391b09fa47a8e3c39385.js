ed141962b362ecd6e0800006c91315c0a9602d11
Alina-Nahdi1 <nahdi.alina.khanam@gmAIL.COM>
2020-06-21 08:26:30 +0530

    Updates

---
+++ precache-manifest.7b3f8304160f391b09fa47a8e3c39385.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657ff8988c8b46ea35eefc5da260ea0b",
+    "url": "/AlinaWeather/index.html"
+  },
+  {
+    "revision": "f08c611020d5a8599f46",
+    "url": "/AlinaWeather/static/css/main.8165ba6d.chunk.css"
+  },
+  {
+    "revision": "7fdc5c4d1349c480283a",
+    "url": "/AlinaWeather/static/js/2.34b38748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlinaWeather/static/js/2.34b38748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08c611020d5a8599f46",
+    "url": "/AlinaWeather/static/js/main.25fa83de.chunk.js"
+  },
+  {
+    "revision": "9decbc767ab5ccb6a684",
+    "url": "/AlinaWeather/static/js/runtime-main.5255b0f5.js"
+  },
+  {
+    "revision": "139d90caa2703c59dfa734ecc481db46",
+    "url": "/AlinaWeather/static/media/1.139d90ca.jpg"
+  },
+  {
+    "revision": "ffa516d7bd38a188ca61f55c7fce2232",
+    "url": "/AlinaWeather/static/media/2.ffa516d7.jpg"
+  }
+]);
